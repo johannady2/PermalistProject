d1822a8c52b87051a6c56c45923d7457e81034ed
johannady2 <johannady2@gmail.com>
2026-04-27 16:04:09 +0900

    select all from dbTable order by id ascending. Also, edited CSS a little bit

--- solution.js
+++ solution.js
@@ -8,8 +8,8 @@ const port = 3000;
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
-  database: "permalist",
-  password: "123456",
+  database: "PermalistProject",
+  password: "ChibaKing82",
   port: 5432,
 });
 db.connect();
